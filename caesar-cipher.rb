4f199393678a11ecfbdbfb9ec680910b7ed83e0e
Nicolás Órdenes <nicordenes@gmail.com>
2021-12-29 19:21:26 -0300

    Add rudimentary unit testing

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -20,3 +20,10 @@ end
 message = "What a string!"
 puts "Message: " + message
 puts "Cipher : " + caesar_cipher(message, 5)
+
+# tests
+puts caesar_cipher('z',0) == 'z'
+puts caesar_cipher('z',1) == 'a'
+puts caesar_cipher('Z',0) == 'Z'
+puts caesar_cipher('Z',1) == 'A'
+puts caesar_cipher(',',1) == ','
