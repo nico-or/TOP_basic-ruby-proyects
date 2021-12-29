1c6ac27d0b8a4a299f2f1e3a62feb1b638199bf9
Nicolás Órdenes <nicordenes@gmail.com>
2021-12-29 19:20:45 -0300

    Add caesar chipher implementation

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -1,5 +1,19 @@
+def char_shift(char, shift, range)
+  relative_shift = (char.ord - range.min.ord + shift) % range.count
+  (range.min.ord + relative_shift).chr
+end
+
 def caesar_cipher(message, shift)
-  message
+  cipher = ""
+  message.each_char do |char|
+    if ('a'..'z').include? char
+      char = char_shift(char, shift, ('a'..'z'))
+    elsif ('A'..'Z').include? char
+        char = char_shift(char, shift, ('A'..'Z'))
+    end
+    cipher += char
+  end
+  cipher
 end
 
 # main
