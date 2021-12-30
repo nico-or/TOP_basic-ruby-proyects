ca4fce72d4cb5b34e5788688a19e405780e1abe6
Nicolás Órdenes <nicordenes@gmail.com>
2021-12-29 22:57:37 -0300

    Add stock-picker implementation

--- stock-picker.rb
+++ stock-picker.rb
@@ -1,5 +1,15 @@
 def stock_picker(array)
-  array
+  max_profit = 0
+  out = []
+  array.each_with_index do |price1, idx|
+    price2 = array[idx + 1, array.length].max
+    # && fixes error at last iteration when price2 = nil
+    if price2 && (price2 - price1) > max_profit
+      out = [idx, array.index(price2)]
+      max_profit = price2 - price1
+    end
+  end
+  out
 end
 
 # Test
