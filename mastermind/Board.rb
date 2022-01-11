0ae8a332b5130f098a1e8c7427ef9a3f751b1d7c
Nicolás Órdenes <nicordenes@gmail.com>
2022-01-11 12:36:55 -0300

    Move player role selection to Game

--- mastermind/Board.rb
+++ mastermind/Board.rb
@@ -1,14 +1,13 @@
 class Board
-  attr_reader :secret_code, :code_maker, :code_breaker
-  def initialize(players)
-    puts "New Board"
+  attr_reader :secret_code
+  def initialize(code_maker)
+    puts "\nNew Board"
     @moves = []
     @game_over = false
-    set_players_roles(players)
     set_secret_code(code_maker)
   end
 
-  def new_move
+  def new_move(code_breaker)
     move = Move.new(code_breaker)
     move.set_feeback(secret_code)
     @game_over = true if move.feedback == 'oooo'
@@ -21,26 +20,6 @@ class Board
   end
 
   private
-
-  def set_players_roles(players)
-    puts "\nWhich player will be the Code Maker?"
-    players.each.with_index { |player, i| puts "#{i+1}) #{player.name}" }
-
-    input_getter = UserInput::InputGetter.new
-    input_getter.set_getter { gets.chomp }
-    input_getter.set_validator { |i| %w{1 2}.include? i }
-
-    case '2'#input_getter.get_input
-    when '1'
-      @code_maker = players[0]
-      @code_breaker = players[1]
-    when '2'
-      @code_maker = players[1]
-      @code_breaker = players[0]
-    end
-    puts "Code Maker: #{code_maker.name}"
-    puts "Code Breaker: #{code_breaker.name}"
-  end
 
   def set_secret_code(player)
     print "\nCodeMaker #{player.name}, enter the secret code: "
--- mastermind/Game.rb
+++ mastermind/Game.rb
@@ -3,11 +3,14 @@ require_relative 'Player'
 require_relative 'Move'
 require_relative 'Mastermind'
 require_relative 'UserInput'
+
 class Game
+  attr_reader :players, :code_maker, :code_breaker
   def initialize
     @players = []
-    select_game_type
+    set_game_type
     greet_players
+    set_players_roles(players)
   end
 
   def play
@@ -18,15 +21,16 @@ class Game
   private
 
   def round
-    @board = Board.new(@players)
+    @board = Board.new(code_maker)
     loop do
-      @board.new_move
+      @board.new_move(code_breaker)
       break if @board.game_over?
     end
-    puts "Round Over"
+
+    puts "\nRound Over"
   end
 
-  def select_game_type
+  def set_game_type
     add_human
     add_computer
   end
@@ -40,9 +44,31 @@ class Game
   end
 
   def greet_players
+    puts ""
     @players.each do |player|
       puts "Welcome #{player.name}"
     end
+  end
+
+  def set_players_roles(players)
+    puts "\nWhich player will be the Code Maker?"
+    players.each.with_index { |player, i|
+      puts "#{i+1}) #{player.name}" }
+
+    input_getter = UserInput::InputGetter.new
+    input_getter.set_getter { gets.chomp }
+    input_getter.set_validator { |i| %w{1 2}.include? i }
+
+    case '2'#input_getter.get_input
+    when '1'
+      @code_maker = players[0]
+      @code_breaker = players[1]
+    when '2'
+      @code_maker = players[1]
+      @code_breaker = players[0]
+    end
+    puts "\nCode Maker: #{code_maker.name}"
+    puts "Code Breaker: #{code_breaker.name}"
   end
 
   def game_over
