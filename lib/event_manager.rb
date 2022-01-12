39a2ca688b6ae5f10f7db2830f3926eda4facf85
Nicolás Órdenes <nicordenes@gmail.com>
2022-01-12 19:11:14 -0300

    Add google civic api call and parsing

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,4 +1,8 @@
 require 'csv'
+require 'open-uri'
+require 'json'
+
+GOOGLE_CIVIC_API_KEY = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
 
 data = CSV.open(
   'event_attendees.csv',
@@ -10,7 +14,24 @@ def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5,'0')[0..4]
 end
 
+def legislator_by_zipcode(zipcode)
+  url = "https://www.googleapis.com/civicinfo/v2/representatives?address=#{zipcode}&levels=country&roles=legislatorUpperBody&roles=legislatorLowerBody&key=#{GOOGLE_CIVIC_API_KEY}"
+
+  begin
+    response = JSON.load(URI.open(url))
+    names = response['officials'].map { |i| i['name'] }
+    names.join(', ')
+  rescue => exception
+    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  end
+end
+
 until data.eof?
-  zipcode = data.readline[:zipcode]
-  puts "#{zipcode.to_s.rjust(10)} → #{clean_zipcode(zipcode)}"
+  row = data.readline
+
+  attendee_name = row[:first_name]
+  zipcode = clean_zipcode(row[:zipcode])
+  legislator_names = legislator_by_zipcode(zipcode)
+
+  puts "#{attendee_name} #{zipcode} #{legislator_names}"
 end
