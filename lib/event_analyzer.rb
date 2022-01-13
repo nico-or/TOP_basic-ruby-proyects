7dc9b5c74cf039cc61351bb1b87eaa5c63ab1385
Nicolás Órdenes <nicordenes@gmail.com>
2022-01-12 21:11:08 -0300

    Add frequency counter

--- lib/event_analyzer.rb
+++ lib/event_analyzer.rb
@@ -14,10 +14,47 @@ def clean_phone_number(number)
   end
 end
 
-until data.eof?
-  row = data.readline
+#until data.eof?
+#  row = data.readline
 
-  name = row[:first_name]
-  number = row[:homephone]
-  puts "#{number.rjust(20)} → #{clean_phone_number(number)}"
+#  name = row[:first_name]
+#  number = row[:homephone]
+  #puts "#{number.rjust(20)} → #{clean_phone_number(number)}"
+#end
+
+#data.rewind
+
+def parse_date(date)
+  DateTime.strptime(date, '%m/%d/%y %H:%M')
 end
+
+regdates = data.readlines.map { |i| parse_date(i[:regdate]) }
+
+
+#reg_day_count = Hash.new(0)
+#reg_hour_count = Hash.new(0)
+#regdates.each do |date|
+#  reg_day_count[date.wday] += 1
+#  reg_hour_count[date.hour] += 1
+#end
+
+def frequency_counter(array, &block)
+  array.map {|i| block.call(i) }.
+  group_by(&:itself).
+  each_with_object({}) { |(k,v),h| h[k] = v.size }
+end
+
+reg_hour_count = frequency_counter(regdates) {|i| i.hour}
+reg_day_count = frequency_counter(regdates) {|i| i.wday }
+
+# ordered by key
+freq_hour_sorted = reg_hour_count.to_a.sort { |a,b| a[0] <=> b[0] }
+freq_day_sorted = reg_day_count.to_a.sort { |a,b| a[0] <=> b[0] }
+
+freq_hour_sorted.each {|hour, freq| puts "Hour: #{hour} Freq: #{freq}"}
+puts "Most Frequent hour: #{freq_hour_sorted.sort{|a,b|a[1]<=>b[1]}.last[0]}"
+
+puts " "
+
+freq_day_sorted.each {|i, freq| puts "Day: #{Date::DAYNAMES[i]} Freq: #{freq}"}
+puts "Most Frequent day: #{Date::DAYNAMES[freq_day_sorted.sort{|a,b|a[1]<=>b[1]}.last[0]]}"
