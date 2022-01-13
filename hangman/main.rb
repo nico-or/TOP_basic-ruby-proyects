3308956d120c85ef13ee7f3e15ebdef0ed12fcdf
Nicolás Órdenes <nicordenes@gmail.com>
2022-01-13 15:35:14 -0300

    Add dictionary class

--- hangman/main.rb
+++ hangman/main.rb
@@ -1,10 +1,32 @@
+class Dictionary
+  attr_reader :words
+
+  def initialize(filename = '5desk.txt')
+    @words = File.open(filename, 'r').readlines.map { |i| i.chomp.downcase }
+  end
+
+  def get_word
+    words_in_range.sample
+  end
+
+  private
+
+  def words_in_range(min = 5, max = 12)
+    @words.filter { |i| i.length.between?(min, max) }
+  end
+end
+
 class Game
+  attr_reader :dictionary, :secret_word
+
   def initialize
     puts "\nNew Game"
+    @dictionary = Dictionary.new('sample.txt')
+    @secret_word = dictionary.get_word
   end
 
   def play
-    puts "From play"
+    puts @secret_word
   end
 end
 
