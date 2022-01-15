c7eb34dcd302e643ea55a22cdeae400145b2237e
Nicolás Órdenes <nicordenes@gmail.com>
2022-01-15 17:02:17 -0300

    Add my_each_with_index

--- custom_enumerables.rb
+++ custom_enumerables.rb
@@ -7,7 +7,18 @@ module Enumerable
     end
     self
   end
+
+  def my_each_with_index
+    index = 0
+    until index == length
+      yield(self[index], index)
+      index += 1
+    end
+    self
+  end
 end
+
+blank_proc = proc { }
 
 # my_each test
 puts "Testing my_each"
@@ -15,4 +26,12 @@ array = (0..4).to_a
 print_block = proc { |i| print "#{i} " }
 p array.my_each(&print_block)
 p array.each(&print_block)
-puts array.my_each(&print_block) == array.each(&print_block)
+puts array.my_each(&blank_proc) == array.each(&blank_proc)
+
+# my_each_with_index test
+puts "Testing my_each_with_index"
+array = ('a'..'e').to_a
+print_with_index_block = proc { |val, idx| puts "index: #{idx} value: #{val}" }
+p array.my_each_with_index(&print_with_index_block)
+p array.each_with_index(&print_with_index_block)
+puts array.my_each_with_index(&blank_proc) == array.each_with_index(&blank_proc)
