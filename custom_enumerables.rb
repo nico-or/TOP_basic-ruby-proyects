c4bbeeab442cbbf75d4e786ce5a4d4f2dbc8a5ce
Nicolás Órdenes <nicordenes@gmail.com>
2022-01-15 16:57:11 -0300

    Add my_each

--- custom_enumerables.rb
+++ custom_enumerables.rb
@@ -1,2 +1,18 @@
 module Enumerable
+  def my_each
+    index = 0
+    until index == length
+      yield self[index]
+      index += 1
+    end
+    self
+  end
 end
+
+# my_each test
+puts "Testing my_each"
+array = (0..4).to_a
+print_block = proc { |i| print "#{i} " }
+p array.my_each(&print_block)
+p array.each(&print_block)
+puts array.my_each(&print_block) == array.each(&print_block)
